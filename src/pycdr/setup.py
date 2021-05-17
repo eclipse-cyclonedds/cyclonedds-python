bc02ca5e317ed0501c4fa053ded8e4838ba73ce8
Thijs Miedema <thijs.miedema@adlinktech.com>
2021-05-17 13:33:39 +0200

    Update PyCDR packaging strategy

--- src/pycdr/setup.py
+++ src/pycdr/setup.py
@@ -10,46 +10,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
 """
+from setuptools import setup
 
-import os
-import sys
-from setuptools import setup, find_packages
-
-
-if sys.version_info < (3, 6):
-    sys.exit("This package cannot be installed in Python version 3.5 or lower.")
-elif sys.version_info < (3, 7):
-    # We are in any Python 3.6 version
-    REQUIRES = ['dataclasses==0.8', 'typing-extensions==3.7.4.3', 'typing-inspect==0.6.0']
-elif sys.version_info < (3, 9):
-    # We are in any Python 3.7 or 3.8 version
-    REQUIRES = ['typing-extensions==3.7.4.3']
-else:
-    # We are in any Python 3.9 or 3.10 (maybe higher?) version, no requirements
-    REQUIRES = []
-
-
-setup(
-    name='pycdr',
-    version='0.1.5',
-    description='Python CDR serialization',
-    install_requires=REQUIRES,
-    author='Thijs Miedema',
-    author_email='thijs.miedema@adlinktech.com',
-    url="https://github.com/thijsmie/cdds-py",
-    project_urls={
-        "Bug Tracker": "https://github.com/thijsmie/cdds-py/issues"
-    },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Operating System :: OS Independent"
-    ],
-    packages=find_packages(exclude=("tests", "examples")),
-    python_requires='>=3.6'
-)
+setup()

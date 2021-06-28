46370a2fd568f25e59d198300a9214bcfa9cc266
yixianlv <yixian.lyu@adlinktech.com>
2021-06-28 18:18:20 +0200

    Bugs fixed and some restructure in ddsls & add test for ddsls
    Signed-off-by: yixianlv <yixian.lyu@adlinktech.com>

---
+++ src/cyclonedds/tests/test_ddsls.py
@@ -0,0 +1,574 @@
+import pytest
+import json
+import time
+import signal
+import os
+import gc
+
+import subprocess
+
+from cyclonedds.domain import DomainParticipant
+from cyclonedds.topic import Topic
+from testtopics import Message
+from cyclonedds.pub import DataWriter
+from cyclonedds.sub import DataReader
+from cyclonedds.core import Qos, Policy, WaitSet, ReadCondition, ViewState, InstanceState, SampleState
+
+
+# Helper functions
+
+def run_ddsls(args, timeout=10):
+    ddsls_process = subprocess.Popen(["ddsls.py"] + args,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE,
+                                     )
+
+    try:
+        stdout, stderr = ddsls_process.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired as e:
+        ddsls_process.kill()
+        raise e
+
+    return {
+        "stdout": stdout.decode(),
+        "stderr": stderr.decode(),
+        "status": ddsls_process.returncode
+    }
+
+
+def start_ddsls_watchmode(args):
+    ddsls_process = subprocess.Popen(["ddsls.py", "--watch"] + args,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE,
+                                     )
+    return ddsls_process
+
+
+def stop_ddsls_watchmode(ddsls_process, timeout=10):
+    ddsls_process.send_signal(signal.SIGINT)
+
+    try:
+        stdout, stderr = ddsls_process.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired as e:
+        ddsls_process.kill()
+        raise e
+
+    return {
+        "stdout": stdout.decode(),
+        "stderr": stderr.decode(),
+        "status": ddsls_process.returncode
+    }
+
+
+# Tests
+
+def test_participant_empty():
+    data = run_ddsls(["-t", "dcpsparticipant"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == ""
+
+
+def test_participant_empty_json():
+    data = run_ddsls(["--json", "-t", "dcpsparticipant"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == "[\n]\n"
+
+
+def test_publication_empty():
+    data = run_ddsls(["-t", "dcpspublication"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == ""
+
+
+def test_publication_empty_json():
+    data = run_ddsls(["--json", "-t", "dcpspublication"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == "[\n]\n"
+
+
+def test_subscription_empty():
+    data = run_ddsls(["-t", "dcpssubscription"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == ""
+
+
+def test_subscription_empty_json():
+    data = run_ddsls(["--json", "-t", "dcpssubscription"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == "[\n]\n"
+
+
+def test_all_empty():
+    data = run_ddsls(["-a"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == ""
+
+
+def test_all_empty_json():
+    data = run_ddsls(["--json", "-a"])
+
+    assert data["status"] == 0
+    assert data["stdout"] == "[\n]\n"
+
+
+def test_participant_reported():
+    dp = DomainParticipant(0)
+    time.sleep(0.5)
+
+    data = run_ddsls(["-t", "dcpsparticipant"])
+
+    time.sleep(0.5)
+
+    assert str(dp.guid) in data["stdout"]
+
+
+def test_participant_json_reported():
+    dp = DomainParticipant(0)
+    time.sleep(0.5)
+
+    data = run_ddsls(["-t", "dcpsparticipant", "--json"])
+
+    time.sleep(0.5)
+
+    assert str(dp.guid) in data["stdout"]
+
+
+def test_participant_watch_reported():
+    ddsls = start_ddsls_watchmode(["-t", "dcpsparticipant"])
+
+    time.sleep(0.5)
+
+    dp = DomainParticipant(0)
+
+    time.sleep(0.5)
+
+    data = stop_ddsls_watchmode(ddsls)
+
+    assert str(dp.guid) in data["stdout"]
+
+
+def test_participant_json_watch_reported():
+    ddsls = start_ddsls_watchmode(["--json", "-t", "dcpsparticipant"])
+
+    time.sleep(0.5)
+
+    dp = DomainParticipant(0)
+
+    time.sleep(0.5)
+
+    data = stop_ddsls_watchmode(ddsls)
+
+    assert str(dp.guid) in data["stdout"]
+
+
+def test_subscription_reported():
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dr = DataReader(dp, tp)
+    time.sleep(1)
+
+    data = run_ddsls(["-t", "dcpssubscription"])
+
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+    assert str(dr.get_qos()) in data["stdout"]
+
+
+def test_subscription_json_reported():
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dr = DataReader(dp, tp)
+    time.sleep(1)
+
+    data = run_ddsls(["--json", "-t", "dcpssubscription"])
+    json_data = json.loads(data["stdout"])
+
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+
+    reader_check = False
+    for sample in json_data:
+        for val in sample["value"]:
+            if val["key"] == str(dr.guid):
+                assert (dr.get_qos()).asdict() == val["qos"]
+                reader_check = True
+    assert reader_check
+
+
+def test_subscription_watch_reported():
+    ddsls = start_ddsls_watchmode(["-t", "dcpssubscription"])
+
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dr = DataReader(dp, tp)
+
+    time.sleep(1)
+
+    data = stop_ddsls_watchmode(ddsls)
+
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+    assert str(dr.get_qos()) in data["stdout"]
+
+
+def test_subscription_json_watch_reported():
+    ddsls = start_ddsls_watchmode(["--json", "-t", "dcpssubscription"])
+
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dr = DataReader(dp, tp)
+
+    time.sleep(1)
+
+    data = stop_ddsls_watchmode(ddsls)
+    json_data = json.loads(data["stdout"])
+
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+
+    reader_check = False
+    for sample in json_data:
+        for val in sample["value"]:
+            if val["key"] == str(dr.guid):
+                assert (dr.get_qos()).asdict() == val["qos"]
+                reader_check = True
+    assert reader_check
+
+
+def test_publication_reported():
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    time.sleep(1)
+
+    data = run_ddsls(["-t", "dcpspublication"])
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+    assert str(dw.get_qos()) in data["stdout"]
+
+
+def test_publication_json_reported():
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    time.sleep(1)
+
+    data = run_ddsls(["--json", "-t", "dcpspublication"])
+    json_data = json.loads(data["stdout"])
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+
+    writer_check = False
+    for sample in json_data:
+        for val in sample["value"]:
+            if val["key"] == str(dw.guid):
+                assert (dw.get_qos()).asdict() == val["qos"]
+                writer_check = True
+    assert writer_check
+
+
+def test_publication_watch_reported():
+    ddsls = start_ddsls_watchmode(["-t", "dcpspublication"])
+
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+
+    time.sleep(1)
+
+    data = stop_ddsls_watchmode(ddsls)
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+    assert str(dw.get_qos()) in data["stdout"]
+
+
+def test_publication_json_watch_reported():
+    ddsls = start_ddsls_watchmode(["--json", "-t", "dcpspublication"])
+
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+
+    time.sleep(1)
+
+    data = stop_ddsls_watchmode(ddsls)
+    json_data = json.loads(data["stdout"])
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+
+    writer_check = False
+    for sample in json_data:
+        for val in sample["value"]:
+            if val["key"] == str(dw.guid):
+                assert (dw.get_qos()).asdict() == val["qos"]
+                writer_check = True
+    assert writer_check
+
+
+def test_all_entities_reported():
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    dr = DataReader(dp, tp)
+    time.sleep(1)
+
+    data = run_ddsls(["-a"])
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+    assert str(dw.get_qos()) in data["stdout"]
+    assert str(dr.get_qos()) in data["stdout"]
+
+
+def test_all_entities_json_reported():
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    dr = DataReader(dp, tp)
+    time.sleep(1)
+
+    data = run_ddsls(["--json", "-a"])
+    json_data = json.loads(data["stdout"])
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+
+    writer_check, reader_check = False, False
+    for sample in json_data:
+        for val in sample["value"]:
+            if val["key"] == str(dw.guid):
+                assert (dw.get_qos()).asdict() == val["qos"]
+                writer_check = True
+            if val["key"] == str(dr.guid):
+                assert (dr.get_qos()).asdict() == val["qos"]
+                reader_check = True
+    assert reader_check and writer_check
+
+
+def test_all_entities_watch_reported():
+    ddsls = start_ddsls_watchmode(["-a"])
+
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    dr = DataReader(dp, tp)
+
+    time.sleep(1)
+
+    data = stop_ddsls_watchmode(ddsls)
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+    assert str(dw.get_qos()) in data["stdout"]
+    assert str(dr.get_qos()) in data["stdout"]
+
+
+def test_all_entities_watch_json_reported():
+    ddsls = start_ddsls_watchmode(["--json", "-a"])
+
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    dr = DataReader(dp, tp)
+
+    time.sleep(1)
+
+    data = stop_ddsls_watchmode(ddsls)
+    json_data = json.loads(data["stdout"])
+
+    assert str(dw.guid) in data["stdout"]
+    assert str(dr.guid) in data["stdout"]
+    assert str(dp.guid) in data["stdout"]
+    assert tp.name in data["stdout"]
+    assert tp.typename in data["stdout"]
+
+    writer_check, reader_check = False, False
+    for sample in json_data:
+        for val in sample["value"]:
+            if val["key"] == str(dw.guid):
+                assert (dw.get_qos()).asdict() == val["qos"]
+                writer_check = True
+            if val["key"] == str(dr.guid):
+                assert (dr.get_qos()).asdict() == val["qos"]
+                reader_check = True
+    assert reader_check and writer_check
+
+
+def test_write_to_file(tmp_path):
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    dr = DataReader(dp, tp)
+
+    time.sleep(0.5)
+
+    run_ddsls(["--json", "-a", "--filename", tmp_path/"test.json"])
+
+    time.sleep(0.5)
+
+    data = json.load(open(tmp_path/"test.json",))
+
+    assert str(dw.guid) in data["PUBLICATION"]["New"]
+    assert str(dr.guid) in data["SUBSCRIPTION"]["New"]
+    assert str(dp.guid) in data["PARTICIPANT"]["New"]
+    assert tp.name in data["PUBLICATION"]["New"][str(dw.guid)]["topic_name"]
+    assert tp.typename in data["PUBLICATION"]["New"][str(dw.guid)]["type_name"]
+    assert tp.name in data["SUBSCRIPTION"]["New"][str(dr.guid)]["topic_name"]
+    assert tp.typename in data["SUBSCRIPTION"]["New"][str(dr.guid)]["type_name"]
+
+    os.remove(tmp_path/"test.json")
+
+
+def test_write_disposed_data_to_file(tmp_path):
+    ddsls = start_ddsls_watchmode(["--json", "-a", "-w", "--filename", tmp_path/"test_disposed.json"])
+
+    time.sleep(0.5)
+
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp)
+    dr = DataReader(dp, tp)
+
+    disposed_data = {
+        "dp.guid": str(dp.guid),
+        "tp.name": tp.name,
+        "tp.typename": tp.typename,
+        "dw.guid": str(dw.guid),
+        "dr.guid": str(dr.guid)
+    }
+    time.sleep(2)
+
+    waitset = WaitSet(dp)
+    cond = ReadCondition(dr, ViewState.Any | InstanceState.NotAliveDisposed | SampleState.Any)
+    waitset.attach(cond)
+
+    del dp
+    gc.collect()
+    time.sleep(0.5)
+
+    stop_ddsls_watchmode(ddsls)
+
+    time.sleep(0.5)
+
+    data = json.load(open(tmp_path/"test_disposed.json",))
+
+    dw_guid = disposed_data["dw.guid"]
+    dr_guid = disposed_data["dr.guid"]
+
+    assert disposed_data["dp.guid"] not in data["PARTICIPANT"]["New"]
+    assert disposed_data["dp.guid"] in data["PARTICIPANT"]["Disposed"]
+    assert dw_guid not in data["PUBLICATION"]["New"]
+    assert dw_guid in data["PUBLICATION"]["Disposed"]
+    assert dr_guid not in data["SUBSCRIPTION"]["New"]
+    assert dr_guid in data["SUBSCRIPTION"]["Disposed"]
+    assert disposed_data["tp.name"] == data["PUBLICATION"]["Disposed"][dw_guid]["topic_name"]
+    assert disposed_data["tp.name"] == data["SUBSCRIPTION"]["Disposed"][dr_guid]["topic_name"]
+    assert disposed_data["tp.typename"] == data["PUBLICATION"]["Disposed"][dw_guid]["type_name"]
+    assert disposed_data["tp.typename"] == data["SUBSCRIPTION"]["Disposed"][dr_guid]["type_name"]
+
+    os.remove(tmp_path/"test_disposed.json")
+
+
+def test_domain_id():
+    dp = DomainParticipant(0)
+    dp1 = DomainParticipant(33)
+
+    time.sleep(0.5)
+
+    data = run_ddsls(["--id", "33", "-t", "dcpsparticipant"])
+
+    time.sleep(0.5)
+
+    assert str(dp.guid) not in data["stdout"]
+    assert str(dp1.guid) in data["stdout"]
+
+
+def test_qos_change():
+    ddsls = start_ddsls_watchmode(["-a"])
+
+    qos = Qos(Policy.OwnershipStrength(10),
+              Policy.Userdata("Old".encode()))
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp, qos=qos)
+    time.sleep(0.5)
+
+    old_qos = dw.get_qos()
+    time.sleep(0.5)
+
+    new_qos = Qos(Policy.OwnershipStrength(20),
+                  Policy.Userdata("New".encode()))
+    dw.set_qos(new_qos)
+
+    time.sleep(0.5)
+
+    data = stop_ddsls_watchmode(ddsls)
+
+    assert str(old_qos) in data["stdout"]
+    for q in new_qos:
+        assert str(q) in data["stdout"]
+        assert f"{str(old_qos[q])} -> {str(q)}" in data["stdout"]
+
+
+def test_qos_change_in_verbose():
+    ddsls = start_ddsls_watchmode(["-a", "--verbose"])
+
+    qos = Qos(Policy.OwnershipStrength(10),
+              Policy.Userdata("Old".encode()))
+    dp = DomainParticipant(0)
+    tp = Topic(dp, "MessageTopic", Message)
+    dw = DataWriter(dp, tp, qos=qos)
+    time.sleep(0.5)
+
+    old_qos = dw.get_qos()
+    time.sleep(0.5)
+
+    new_qos = Qos(Policy.OwnershipStrength(20),
+                  Policy.Userdata("New".encode()))
+    dw.set_qos(new_qos)
+
+    time.sleep(0.5)
+
+    data = stop_ddsls_watchmode(ddsls)
+
+    assert str(old_qos) in data["stdout"]
+    for q in new_qos:
+        assert str(q) in data["stdout"]
+        assert f"{str(old_qos[q])} -> {str(q)}" in data["stdout"]
+    assert str(dw.get_qos()) in data["stdout"]
--- src/cyclonedds/tools/ddsls.py
+++ src/cyclonedds/tools/ddsls.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sys
 import json
+import datetime
 import argparse
+
 from cyclonedds.domain import DomainParticipant
 from cyclonedds.builtin import (BuiltinDataReader, BuiltinTopicDcpsParticipant,
                                 BuiltinTopicDcpsSubscription, BuiltinTopicDcpsPublication)
@@ -18,104 +20,126 @@ class TopicManager:
         self.enable_view = args.verbose
         self.dp_key = reader.participant.guid
         self.tracked_entities = {}
-        self.tracked_data = {}
+        self.tracked_disposed_entities = {}
         self.qoses = {}
         self.strings = ""
         self.read_cond = ReadCondition(reader, ViewState.Any | InstanceState.Alive | SampleState.NotRead)
         self.disposed_cond = ReadCondition(reader, ViewState.Any | InstanceState.NotAliveDisposed | SampleState.Any)
 
+    # Read and print samples
     def poll(self):
-        samples = self.reader.take(N=100, condition=self.read_cond)
-        disposed_samples = self.reader.take(N=100, condition=self.disposed_cond)
-        if samples:
+        new_samples = []
+        newly_disposed_samples = []
+
+        while True:
+            samples = self.reader.take(N=100, condition=self.read_cond)
+
+            if not samples:
+                break
+
             for sample in samples:
                 if ((self.topic_type == "PARTICIPANT" and sample.key != self.dp_key)
                    or (self.topic_type != "PARTICIPANT" and sample.participant_key != self.dp_key)):
-                    print("\n--- New", self.topic_type, "------------", end="", flush=True)
-                    self.manage_samples(sample)
-                    self.check_qos_changes(sample)
-        if disposed_samples:
+                    self.track_sample(sample)
+                    if self.check_qos_changes(sample):
+                        new_samples.append(sample)
+
+        while True:
+            disposed_samples = self.reader.take(N=100, condition=self.disposed_cond)
+
+            if not disposed_samples:
+                break
+
             for disposed_sample in disposed_samples:
-                print("\n--- Disposed", self.topic_type, "------------", end="", flush=True)
-                self.manage_samples(disposed_sample)
+                newly_disposed_samples.append(self.tracked_entities.get(str(disposed_sample.key), disposed_sample))
+                self.untrack_sample(disposed_sample)
 
-        if self.console_print and self.tracked_entities and (samples or disposed_samples):
-            Output.to_console(self)
+        if self.console_print and (new_samples or newly_disposed_samples):
+            self.to_console(new_samples, newly_disposed_samples)
 
-    def manage_samples(self, sample):
-        if self.topic_type == "PARTICIPANT":
-            self.tracked_entities = {
-                self.topic_type: {
-                    "key": str(sample.key)
-                    }
-                }
-        else:
-            if sample.topic_name is not None:
-                self.tracked_data = {
-                    sample.key: {
-                        "topic_name": sample.topic_name,
-                        "type_name": sample.type_name,
-                        "qoses": sample.qos
-                        }
-                    }
-            self.tracked_entities = {
-                self.topic_type: {
-                    "key": str(sample.key),
-                    "participant_key": str(sample.participant_key),
-                    "topic_name": self.tracked_data[sample.key]["topic_name"],
-                    "type_name": self.tracked_data[sample.key]["type_name"],
-                    "qos": self.tracked_data[sample.key]["qoses"].asdict()
-                    }
-                }
+    # Add new samples to dict
+    def track_sample(self, sample):
+        self.tracked_entities[str(sample.key)] = sample
 
+    # Collect disposed sample to a new dict and delete it from the original dict
+    def untrack_sample(self, sample):
+        if str(sample.key) in self.tracked_entities:
+            self.tracked_disposed_entities[str(sample.key)] = self.tracked_entities[str(sample.key)]
+            del self.tracked_entities[str(sample.key)]
+
+    # Track qos changes
     def check_qos_changes(self, sample):
         key = sample.key
         if self.qoses.get(key, 0) == 0:
             self.qoses[key] = sample.qos
         elif self.qoses[key] != sample.qos:
+            print(f"""\n\033[1mQos changed on topic '{sample.topic_name}' {self.topic_type.lower()}:\033[0m
+                  \r key = {sample.key}""")
             for i in self.qoses[key]:
                 if self.qoses[key][i] != sample.qos[i]:
-                    print("\n\033[1mQos changed:\033[0m\nfor the", self.topic_type, "of topic '",
-                          sample.topic_name, "'", "\nwith key =", sample.key, ":\n ",
-                          "\033[1m", str(self.qoses[key][i]), "->", str(sample.qos[i]), "\033[0m")
+                    print(f"\033[1m {str(self.qoses[key][i])} -> {str(sample.qos[i])}\033[0m")
             self.qoses[key] = sample.qos
-            if not self.enable_view and self.console_print:
-                self.tracked_entities = 0
 
+            if not self.enable_view:  # If not verbose, don't print the qos changed sample data
+                sample = None
+        return sample
+
+    # Wait for new / disposed samples
     def add_to_waitset(self, waitset):
         waitset.attach(self.read_cond)
         waitset.attach(self.disposed_cond)
 
-
-class Output(TopicManager):
-    def to_file(self, obj, fp):
-        for result in obj:
-            if result.tracked_entities:
-                if result.enable_json:
-                    json.dump(result.tracked_entities, fp, indent=4)
-                else:
-                    Output.in_strings(result)
-                    fp.write(str(result.strings))
-
-    def to_console(self):
-        if self.enable_json:
-            json.dump(self.tracked_entities, sys.stdout, indent=4)
+    # Format sample value for console printing in JSON
+    def format_value(self, sample):
+        if self.topic_type == "PARTICIPANT":
+            return {
+                "key": str(sample.key),
+            }
         else:
-            Output.in_strings(self)
-            print(self.strings)
+            return {
+                "key": str(sample.key),
+                "participant_key": str(sample.participant_key),
+                "topic_name": str(sample.topic_name),
+                "type_name": str(sample.type_name),
+                "qos": sample.qos.asdict()
+            }
 
-    def in_strings(self):
-        for topic, data in self.tracked_entities.items():
-            self.strings = "\n" + topic
-            for name, value in data.items():
-                if name == "qos":
-                    self.strings += "\n " + name + ":"
-                    for i in value.items():
-                        self.strings += "\n  " + str(i)
-                else:
-                    self.strings += "\n " + name + ":" + str(value)
+    # Format sample for writing to file in JSON
+    def as_dict(self):
+        return {
+            "New": {k: self.format_value(v) for k, v in self.tracked_entities.items()},
+            "Disposed": {k: self.format_value(v) for k, v in self.tracked_disposed_entities.items()}
+        }
+
+    # Print to console in JSON or non-JSON format
+    def to_console(self, new_samples, newly_disposed_samples):
+        if self.enable_json:
+            if new_samples:
+                JsonWriter.write({
+                    "type": self.topic_type,
+                    "event": "new",
+                    "value": [self.format_value(sample) for sample in new_samples]
+                })
+            if newly_disposed_samples:
+                JsonWriter.write({
+                    "type": self.topic_type,
+                    "event": "disposed",
+                    "value": [self.format_value(sample) for sample in newly_disposed_samples]
+                })
+        else:
+            if new_samples:
+                print(f"\n-- New {self.topic_type} --")
+                for sample in new_samples:
+                    print("\n".join(f" {item}: {sample.__dict__[item]}" for item in sample.__dict__
+                          if item != "sample_info") + "\n")
+            if newly_disposed_samples:
+                print(f"\n-- Disposed {self.topic_type} --")
+                for sample in newly_disposed_samples:
+                    print("\n".join(f" {item}: {sample.__dict__[item]}" for item in sample.__dict__
+                          if item != "sample_info") + "\n")
 
 
+# Check the topic(s)
 def parse_args(args):
     if args.topic:
         if args.topic == "dcpsparticipant":
@@ -135,7 +159,7 @@ def parse_args(args):
 def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--id", type=int, help="Define the domain participant id", default=0)
-    parser.add_argument("-f", "--filename", type=str, help="Write to file")
+    parser.add_argument("-f", "--filename", type=str, help="Write results to file in JSON format")
     parser.add_argument("-j", "--json", action="store_true", help="Print output in JSON format")
     parser.add_argument("-w", "--watch", action="store_true", help="Watch for data reader & writer & qoses changes")
     parser.add_argument("-v", "--verbose", action="store_true",
@@ -148,6 +172,18 @@ def create_parser():
     return args
 
 
+# Format samples in JSON
+class JsonWriter:
+    first = True
+
+    @classmethod
+    def write(cls, data):
+        if not cls.first:
+            print(",")
+        cls.first = False
+        json.dump(data, sys.stdout, indent=4)
+
+
 def main():
     managers = []
     args = create_parser()
@@ -156,8 +192,14 @@ def main():
     waitset = WaitSet(dp)
 
     for topic_type, topic in topics:
+        # Create TopicManager for each topic
         managers.append(TopicManager(BuiltinDataReader(dp, topic), topic_type, args))
         managers[-1].add_to_waitset(waitset)
+
+    if not args.filename and args.json:
+        print("[")
+
+    # Watchmode
     if args.watch:
         try:
             while True:
@@ -166,15 +208,23 @@ def main():
                     manager.poll()
         except KeyboardInterrupt:
             pass
+    # Non-watchmode
     else:
-        for manager in managers:
-            manager.poll()
+        time_start = datetime.datetime.now()
+        while datetime.datetime.now() < time_start + datetime.timedelta(seconds=1):
+            for manager in managers:
+                manager.poll()
 
+    if not args.filename and args.json:
+        print("]")
+
+    # Write to file
     if args.filename:
         try:
             with open(args.filename, 'w') as f:
-                Output.to_file(manager, managers, f)
-                print("\nResults have been written to file", args.filename, "\n")
+                data = {manager.topic_type: manager.as_dict() for manager in managers}
+                json.dump(data, f, indent=4)
+                print(f"\nResults have been written to file {args.filename}\n")
         except OSError:
             print("could not open file")
             return 1

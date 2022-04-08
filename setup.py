885330b88ae2f204bbda15a49459b70316173442
Thijs Miedema <mail@tmiedema.com>
2022-04-08 09:58:49 +0200

    Update README and set project status to Production in setup.py

--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ setup(
         "idl", "middleware", "ros"
     ],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",

17c6fb4a71606f298873a48fa2260fd34ccc73ce
Erik Boasson <eb@ilities.com>
2022-05-10 16:18:32 +0200

    Add missing copyright blurbs
    Signed-off-by: Erik Boasson <eb@ilities.com>

--- buildhelp/bdist_wheel.py
+++ buildhelp/bdist_wheel.py
@@ -1,3 +1,15 @@
+"""
+ * Copyright(c) 2021 to 2022 ZettaScale Technology and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Eclipse Distribution License
+ * v. 1.0 which is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR BSD-3-Clause
+"""
+
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 from cyclone_search import find_cyclonedds
 from pathlib import Path

ae0ae0af2d45da123a5dbaa4e994f584bafa16f1
Dennis Potman <dennis@zettascale.tech>
2022-07-12 12:09:58 +0200

    Remove dds_find_topic from api
    This removes the find_topic function from the api. As a consequence
    of the changes in the C dds_find_topic function in Cyclone PR 1308,
    finding a topic now requires also a type information object. As the
    type information is internal in the python api and should not be
    exposed to the user, other ways of getting topic information are
    preferred.
    
    Signed-off-by: Dennis Potman <dennis@zettascale.tech>

--- tests/test_participant.py
+++ tests/test_participant.py
@@ -10,15 +10,3 @@ from support_modules.testtopics import Message
 def test_create_participant():
     dp = DomainParticipant(0)
     assert isgoodentity(dp)
-
-
-def test_find_topic():
-    dp = DomainParticipant(0)
-    tp = Topic(dp, "Message", Message)
-
-    assert isgoodentity(tp)
-    
-    xtp = dp.find_topic("Message")
-
-    assert xtp.typename == tp.typename
-    assert xtp.name == tp.name

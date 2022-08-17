492de0bf602039508a0c8d66b9d30b6be3042a73
Will S <willstott101@gmail.com>
2022-08-17 16:26:01 +0100

    Support un-assigning listeners in Entity, and fix bug with reassignment.

--- cyclonedds/core.py
+++ cyclonedds/core.py
@@ -474,23 +474,33 @@ class Entity(DDS):
         """
         return self._listener.copy() if self._listener else Listener()
 
-    def set_listener(self, listener: "Listener") -> None:
+    def set_listener(self, listener: Optional["Listener"]) -> None:
         """Update the listener for this object. If a listener already exist for this object only the fields you explicitly
-        have set on your new listener are overwritten.
+        have set on your new listener are overwritten. Passing None will remove this entity's Listener.
+
+        Future changes to the passed Listener object will not affect the Listener associated with this Entity.
 
         Parameters
         ----------
-        listener : Listener
-            The listener object to use.
+        listener :
+            The listener object to use, or None to remove the current listener from this Entity.
 
         Raises
         ------
         DDSException
         """
-        if self._listener != listener:
-            listener.copy_to(self._listener)
+        if listener is not None:
+            if self._listener is not None:
+                if self._listener != listener:
+                    listener.copy_to(self._listener)
+            else:
+                self._listener = listener.copy()
+            ref = self._listener._ref
+        else:
+            ref = None
+            self._listener = None
 
-        ret = self._set_listener(self._ref, listener._ref)
+        ret = self._set_listener(self._ref, ref)
         if ret == 0:
             return
         raise DDSException(ret, f"Occurred when setting the Listener for {repr(self)}")

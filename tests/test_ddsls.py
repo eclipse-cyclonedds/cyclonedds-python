dda46fe12b5180fd91ac135d7f798d14298e438f
Sven Trittler <sven@trittler.net>
2024-06-03 21:19:07 +0200

    fix build type

--- tests/test_ddsls.py
+++ tests/test_ddsls.py
@@ -19,6 +19,10 @@ from cyclonedds.core import Qos, Policy
 from support_modules.testtopics import Message
 
 
+if sys.platform.startswith("win"):
+    pytest.skip("qos is unstable on windows", allow_module_level=True)
+
+
 # Helper functions
 
 def run_ddsls(args, timeout=10):

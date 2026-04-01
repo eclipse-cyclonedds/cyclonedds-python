636a0713f9d37b6a2d30e12449210084e3be658f
Troy Karan Harrison <troy.harrison@zettascale.tech>
2026-04-01 09:06:52 +0200

    Increment classifiers in setup.py to include 3.14

--- setup.py
+++ setup.py
@@ -119,6 +119,7 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
         "Operating System :: OS Independent"
     ],
     packages=find_packages(".", include=("cyclonedds", "cyclonedds.*")),
